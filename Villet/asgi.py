36c29114ebefe9108accd2155352cf8188330c3d
GURUKIRAN <114338986+gurukiran-gk@users.noreply.github.com>
2023-09-11 20:45:05 +0530

    Add files via upload

---
+++ Villet/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Villet project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Villet.settings')
+
+application = get_asgi_application()
